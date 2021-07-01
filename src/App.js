3139ad7da8e0c734b90c4ac4268623cf4d1c1ebe
Mark <mark.nelubin@gmail.com>
2021-07-01 20:17:11 +0300

    added form that sends data of user inputs to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -24,6 +26,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log(expense);
+  }
+
   // return React.createElement(
   //   "div",
   //   {},
@@ -33,7 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
