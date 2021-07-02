607d6cbf434421691a71a301be13c485a6e82e5d
Mark <mark.nelubin@gmail.com>
2021-07-02 19:43:55 +0300

    added filter functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,31 +10,24 @@ const Expenses = props => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
-
+  
+  // create filtered list: for every expense get year in string and compare it with filtered year
+  const filteredExpenses = props.items.filter(expense => 
+    expense.date.getFullYear().toString() === filteredYear);
+  
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {/*  add our filtered list to rendering  */}
+        {filteredExpenses.map((expense, index) => (
+          <ExpenseItem
+            key={index}
+            title={expense.title} 
+            amount={expense.amount} 
+            date={expense.date} 
+          />
+        ))}
       </Card>
     </div>
   );
